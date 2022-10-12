b566d7d6e46b2cc7854d6d45371f1601c576f45e
Arun Kumar <arun.kumar@iiht.co.in>
2022-10-12 17:30:26 +0530

    angular basics, root module, components, parent-child, data-binding

--- angular/tutorial/src/main.ts
+++ angular/tutorial/src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // import { AppModule } from "./01-structure/app.module";
-import { AppModule } from "./02-components/app.module";
+// import { AppModule } from "./02-components/app.module";
+// import { AppModule } from "./03-parent-child/app.module";
+import { AppModule } from "./04-databinding/app.module";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
