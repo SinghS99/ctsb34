aa3007810518c4b127e49af49308403f6e5e0ae3
Arun Kumar <arun.kumar@iiht.co.in>
2022-10-12 15:47:52 +0530

    angular components

--- angular/tutorial/src/main.ts
+++ angular/tutorial/src/main.ts
@@ -1,1 +1,5 @@
-console.log("hello")
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+// import { AppModule } from "./01-structure/app.module";
+import { AppModule } from "./02-components/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule)
